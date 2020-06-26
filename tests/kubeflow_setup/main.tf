39594899bdf1d9defaf13b67e0b397983a34b44f
Samuel Dumont <samuel@dataroots.io>
2020-06-26 07:48:07 +0200

    Adding the golangci lint config file

---
+++ tests/kubeflow_setup/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = "~> 0.13"
+}
+
+provider "kubernetes" {}
+
+provider "k8s" {}
+
+provider "helm" {}
+
+module "kubeflow" {
+  providers = {
+    kubernetes = kubernetes
+    k8s        = k8s
+    helm       = helm
+  }
+
+  source  = "datarootsio/kubeflow/module"
+  version = "~>0.0"
+
+  ingress_gateway_ip = "10.20.30.40"
+  use_cert_manager   = true
+  domain_name        = "foo.local"
+  letsencrypt_email = "foo@bar.local"
+}
