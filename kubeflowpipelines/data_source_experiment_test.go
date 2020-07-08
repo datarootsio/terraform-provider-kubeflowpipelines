2beccfb1920a4177e6fba453ab4a895d49c9c348
Samuel Dumont <samuel@dataroots.io>
2020-07-08 09:04:28 +0200

    Read the file directly in go

--- kubeflowpipelines/data_source_experiment_test.go
+++ kubeflowpipelines/data_source_experiment_test.go
@@ -37,9 +37,37 @@ resource "kubeflowpipelines_experiment" "test" {
 data "kubeflowpipelines_experiment" "test" {
   id = kubeflowpipelines_experiment.test.id
 }
+`, experimentName, experimentName)
+}
 
-data "kubeflowpipelines_experiment" "test2" {
-	name = "%s"
+func TestAccDataSourceKubeflowPipelinesExperiment_name(t *testing.T) {
+	resourceName := "data.kubeflowpipelines_experiment.test"
+	experimentName := acctest.RandString(6)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceKubeflowPipelinesExperimentName(experimentName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", experimentName),
+					resource.TestCheckResourceAttr(resourceName, "description", fmt.Sprintf("Description %s", experimentName)),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceKubeflowPipelinesExperimentName(experimentName string) string {
+	return fmt.Sprintf(`
+resource "kubeflowpipelines_experiment" "test" {
+  name        = "%s"
+  description = "Description %s"
+}
+
+data "kubeflowpipelines_experiment" "test" {
+  name = "%s"
 }
 `, experimentName, experimentName, experimentName)
 }
