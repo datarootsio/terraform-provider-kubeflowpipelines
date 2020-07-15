ab0db9a96b335d29d4d606850b2bb82193fa565c
Samuel Dumont <samuel@dumont.info>
2020-07-15 16:21:45 +0200

    Adding even more tests (#22)
    * Adding tests for pipeline from URL
    
    * Adding tests for pipeline from URL
    
    * Adding tests for pipeline from URL
    
    * Adding the tests for the url creation
    
    * Adding the tests for the url creation
    
    * gofmt
    
    * Adding tests for the run object
    
    * Adding tests for the run object
    
    * Checking the update
    
    * Adding the update checks

--- kubeflowpipelines/resource_pipeline_test.go
+++ kubeflowpipelines/resource_pipeline_test.go
@@ -35,11 +35,17 @@ func TestAccResourceKubeflowPipelinesPipeline_basic(t *testing.T) {
 				Config: testAccResourceKubeflowPipelinesPipelineUpdate(pipelineName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceName, "name", pipelineName),
-					resource.TestCheckResourceAttr(resourceName, "description", fmt.Sprintf("Description %s updated", pipelineName)),
+					resource.TestCheckResourceAttr(resourceName, "description", fmt.Sprintf("Description %s", pipelineName)),
 				),
 			},
 			{
 				Config: testAccResourceKubeflowPipelinesPipelineFromURL(pipelineNameURL),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceNameURL, "name", pipelineNameURL),
+				),
+			},
+			{
+				Config: testAccResourceKubeflowPipelinesPipelineFromURLUpdate(pipelineNameURL),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(resourceNameURL, "name", pipelineNameURL),
 				),
@@ -97,7 +103,7 @@ func testAccResourceKubeflowPipelinesPipelineUpdate(pipelineName string) string 
 	return fmt.Sprintf(`
 resource "kubeflowpipelines_pipeline" "test" {
 	name        = "%[1]s"
-	description = "Description %[1]s updated"
+	description = "Description %[1]s"
 	file_base64 = "YXBpVmVyc2lvbjogYXJnb3Byb2ouaW8vdjFhbHBoYTEKa2luZDogV29ya2Zsb3cKbWV0YWRhdGE6CiAgZ2VuZXJhdGVOYW1lOiBrdWJlZmxvdy1waXBlbGluZS1mb3ItdGVzdC0KICBhbm5vdGF0aW9uczoge3BpcGVsaW5lcy5rdWJlZmxvdy5vcmcva2ZwX3Nka192ZXJzaW9uOiAwLjUuMSwgcGlwZWxpbmVzLmt1YmVmbG93Lm9yZy9waXBlbGluZV9jb21waWxhdGlvbl90aW1lOiAnMjAyMC0wNy0wNlQxMzozMzoxMi41MTM4NzInLAogICAgcGlwZWxpbmVzLmt1YmVmbG93Lm9yZy9waXBlbGluZV9zcGVjOiAneyJkZXNjcmlwdGlvbiI6ICJUaGlzIHBpcGVsaW5lIGlzIGEgdGVzdCIsICJpbnB1dHMiOiBbeyJuYW1lIjogImtleSJ9LAogICAgICB7Im5hbWUiOiAiZm9vIn1dLCAibmFtZSI6ICJLdWJlZmxvdyBwaXBlbGluZSBmb3IgdGVzdCJ9J30KICBsYWJlbHM6IHtwaXBlbGluZXMua3ViZWZsb3cub3JnL2tmcF9zZGtfdmVyc2lvbjogMC41LjF9CnNwZWM6CiAgZW50cnlwb2ludDoga3ViZWZsb3ctcGlwZWxpbmUtZm9yLXRlc3QKICB0ZW1wbGF0ZXM6CiAgLSBuYW1lOiBrdWJlZmxvdy1waXBlbGluZS1mb3ItdGVzdAogICAgaW5wdXRzOgogICAgICBwYXJhbWV0ZXJzOgogICAgICAtIHtuYW1lOiBrZXl9CiAgICAgIC0ge25hbWU6IGZvb30KICAgIGRhZzoKICAgICAgdGFza3M6CiAgICAgIC0gbmFtZTogc3RlcC0xCiAgICAgICAgdGVtcGxhdGU6IHN0ZXAtMQogICAgICAgIGFyZ3VtZW50czoKICAgICAgICAgIHBhcmFtZXRlcnM6CiAgICAgICAgICAtIHtuYW1lOiBrZXksIHZhbHVlOiAne3tpbnB1dHMucGFyYW1ldGVycy5rZXl9fSd9CiAgICAgICAgICAtIHtuYW1lOiBmb28sIHZhbHVlOiAne3tpbnB1dHMucGFyYW1ldGVycy5mb299fSd9CiAgICAgIC0gbmFtZTogc3RlcC0xLTIKICAgICAgICB0ZW1wbGF0ZTogc3RlcC0xLTIKICAgICAgICBkZXBlbmRlbmNpZXM6IFtzdGVwLTFdCiAgICAgICAgYXJndW1lbnRzOgogICAgICAgICAgcGFyYW1ldGVyczoKICAgICAgICAgIC0ge25hbWU6IGtleSwgdmFsdWU6ICd7e2lucHV0cy5wYXJhbWV0ZXJzLmtleX19J30KICAtIG5hbWU6IHN0ZXAtMQogICAgY29udGFpbmVyOgogICAgICBhcmdzOiBbLS1rZXksICd7e2lucHV0cy5wYXJhbWV0ZXJzLmtleX19JywgLS1mb28sICd7e2lucHV0cy5wYXJhbWV0ZXJzLmZvb319J10KICAgICAgY29tbWFuZDogW2VjaG8sICcnJ3BpcGVsaW5lJycnXQogICAgICBlbnY6CiAgICAgIC0ge25hbWU6IEVOVklST05FTUVOVCwgdmFsdWU6IHN1cGVyZW52fQogICAgICBpbWFnZTogaGVsbG8td29ybGQKICAgICAgaW1hZ2VQdWxsUG9saWN5OiBBbHdheXMKICAgIGlucHV0czoKICAgICAgcGFyYW1ldGVyczoKICAgICAgLSB7bmFtZToga2V5fQogICAgICAtIHtuYW1lOiBmb299CiAgICBtZXRhZGF0YToKICAgICAgbGFiZWxzOiB7cGlwZWxpbmVzLmt1YmVmbG93Lm9yZy9waXBlbGluZS1zZGstdHlwZToga2ZwfQogIGFyZ3VtZW50czoKICAgIHBhcmFtZXRlcnM6CiAgICAtIHtuYW1lOiBrZXl9CiAgICAtIHtuYW1lOiBmb299CiAgc2VydmljZUFjY291bnROYW1lOiBwaXBlbGluZS1ydW5uZXIK"
 	file_format = "yaml"
 	version     = "v0.0.2"
@@ -111,6 +117,16 @@ resource "kubeflowpipelines_pipeline" "test_url" {
 	name        = "%[1]s"
 	url = "https://raw.githubusercontent.com/datarootsio/terraform-provider-kubeflowpipelines/master/tests/kubeflow_setup/pipeline.yaml"
 	version     = "v0.0.1"
+}
+`, pipelineName)
+}
+
+func testAccResourceKubeflowPipelinesPipelineFromURLUpdate(pipelineName string) string {
+	return fmt.Sprintf(`
+resource "kubeflowpipelines_pipeline" "test_url" {
+	name        = "%[1]s"
+	url = "https://raw.githubusercontent.com/datarootsio/terraform-provider-kubeflowpipelines/master/tests/kubeflow_setup/pipeline.yaml"
+	version     = "v0.0.2"
 }
 `, pipelineName)
 }
